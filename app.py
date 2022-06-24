6da0daf889b39012d1f810a14a02aaa1b2e24a16
emil <e.pargevich@mail.ru>
2022-06-24 11:45:03 +0400

    asdasdasd

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from src import app
 from src.__Parents import ContextInitializer
 
 if __name__ == '__main__':
-    # ContextInitializer.ContextInitializer()
+    ContextInitializer.ContextInitializer()
     app.run(debug=True)
